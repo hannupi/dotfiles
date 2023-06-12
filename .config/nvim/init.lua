9d0315fe5a76d263c0770a7cd4b1248ae1e7a559
dft <varahahmo1@gmail.com>
2023-06-12 22:08:53 +0300

    12.6.2023

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -108,7 +108,6 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
