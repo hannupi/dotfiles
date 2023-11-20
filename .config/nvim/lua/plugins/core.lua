874ba520888840250697e1469feffc57cb8b327a
Hannu <varahahmo1@gmail.com>
2023-11-20 20:17:21 +0200

    style, neotree float, tmux conf

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,23 +1,33 @@
 local Util = require("lazyvim.util")
 return {
-  {
-    "h4np/c1-theme.vim",
-    config = function()
-      vim.cmd.colorscheme("c1-theme")
-    end,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    cmd = "Neotree",
-    keys = {
-      {
-        "<leader>fe",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
-        end,
-        desc = "Explorer NeoTree (root dir)",
-      },
-      { "<leader><tab>", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-    },
-  },
+	{
+		"h4np/c1-theme.vim",
+		config = function()
+			vim.cmd.colorscheme("c1-theme")
+		end,
+	},
+
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		cmd = "Neotree",
+		keys = {
+			{
+				"<leader>fe",
+				function()
+					require("neo-tree.command").execute({
+						position = "float",
+						toggle = true,
+						dir = Util.root(),
+					})
+				end,
+				desc = "Explorer NeoTree (root dir)",
+			},
+			{
+				"<leader><tab>",
+				"<leader>fe",
+				desc = "Explorer NeoTree (root dir)",
+				remap = true,
+			},
+		},
+	},
 }
