53abdc2f9b3ec4974a8ecdce47f5a79a5e722e5e
hannupi <varahahmo1@gmail.com>
2024-08-09 13:29:06 +0300

    sudo check for setup script and define interpreter

--- setup_script.sh
+++ setup_script.sh
@@ -1,9 +1,17 @@
-sudo dnf install zsh
-sudo dnf install neovim
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+dnf install zsh
+dnf install neovim
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/InconsolataGo.zip $HOME
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME';echo ".cfg" >> .gitignore
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo ".cfg" >>.gitignore
 git clone --bare git@github.com:hannupi/dotfiles.git $HOME/.cfg
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME -local status.showUntrackedFiles no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
