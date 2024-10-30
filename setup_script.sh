41ed422c3fb9403bbfce6b7b80586db5c2811a2a
hannu <varahahmo1@gmail.com>
2024-10-30 21:55:49 +0200

    fixed setup script

--- setup_script.sh
+++ setup_script.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit 1
-fi
-
-dnf install zsh
-dnf install neovim
+#zsh
+sudo dnf copr enable atim/lazygit -y
+sudo dnf install -y neovim alacritty lazygit fd
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/InconsolataGo.zip $HOME
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-echo ".cfg" >>.gitignore
 git clone --bare git@github.com:hannupi/dotfiles.git $HOME/.cfg
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME -local status.showUntrackedFiles no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+config config --local status.showUntrackedFiles no
