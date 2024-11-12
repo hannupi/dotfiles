b9a0e0a3fa848f1b971f41a86bcb16a9b1e32a72
hannu <varahahmo1@gmail.com>
2024-11-12 20:21:22 +0200

    nvim default to tabs for intendation

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.laststatus = 0
+
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
