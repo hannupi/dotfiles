1a090042ae9f9975f5bededa47a503ffb91ccdab
hannupi <varahahmo1@gmail.com>
2025-01-12 15:53:59 +0200

    add more packages to setup script just in case

--- setup_script.sh
+++ setup_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# sudo dnf install lazygit fd nvim tmux fzf ripgrep go vim alacritty zsh htop yt-dlp qbittorrent vlc
+# sudo dnf copr enable atim/lazygit -y && sudo dnf install -y curl git lazygit fd nvim tmux fzf ripgrep go vim alacritty zsh htop yt-dlp qbittorrent vlc lazygit
 
 EDITOR=vim
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/InconsolataGo.zip
@@ -10,6 +10,6 @@ dconf load / <./.config/gnome-settings.dconf
 cp ./.zshrc.pre-oh-my-zsh ./.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# rust
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && nvm install node
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
