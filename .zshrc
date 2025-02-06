3850faba3df0638a67a13648162181dafa451340
hannupi <varahahmo1@gmail.com>
2025-02-06 18:27:13 +0200

    fixed more stuff related to first time install

--- .zshrc
+++ .zshrc
@@ -118,5 +118,3 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-. "$HOME/.local/bin/env"
--- setup_script.sh
+++ setup_script.sh
@@ -13,12 +13,6 @@ echo "packages"
 sudo dnf copr enable atim/lazygit -y
 sudo dnf install -y curl wget git lazygit fd-find neovim tmux fzf ripgrep go vim alacritty zsh htop yt-dlp qbittorrent vlc ffmpeg
 
-echo "font"
-wget -q "$FONT_URL" -O "$FONT_FILE"
-unzip -o "$FONT_FILE" -d ~/.fonts
-fc-cache -fv
-rm -f "$FONT_FILE"
-
 if [[ -f "$GNOME_SETTINGS" ]]; then
 	echo "Applying GNOME settings..."
 	dconf load / <"$GNOME_SETTINGS"
@@ -26,46 +20,58 @@ else
 	echo "Warning: GNOME settings file not found at $GNOME_SETTINGS. Skipping."
 fi
 
-echo "Configuring Zsh..."
 if [[ -f "$ZSHRC_PRE" ]]; then
+	echo "Configuring Zsh..."
 	cp "$ZSHRC_PRE" "$ZSHRC"
 else
 	echo "Warning: Zsh pre-configuration file not found at $ZSHRC_PRE. Skipping."
 fi
 
-ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-mkdir -p "$ZSH_CUSTOM/plugins"
-if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]]; then
-	echo "Cloning Zsh Autosuggestions plugin..."
-	git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-else
-	echo "Zsh Autosuggestions plugin already installed."
+if [[ ! -f "$HOME/.cargo/bin/rustup" ]]; then
+	echo "rust"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
 
-echo "rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-echo "nvm and node"
 if [[ ! -d "$HOME/.nvm" ]]; then
+	echo "nvm and node"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 	export NVM_DIR="$HOME/.nvm"
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+	nvm install node
 fi
-nvm install node
 
-read -p "Do you want to install Oh My Zsh? (y/n): " INSTALL_OH_MY_ZSH
-if [[ "$INSTALL_OH_MY_ZSH" =~ ^[Yy]$ ]]; then
+if [[ ! -f "$HOME/.fonts/InconsolataGoNerdFont-Regular.ttf" ]]; then
+	echo "font"
+	wget -q "$FONT_URL" -O "$FONT_FILE"
+	unzip -o "$FONT_FILE" -d ~/.fonts
+	fc-cache -fv
+	rm -f "$FONT_FILE"
+fi
+
+if [[ ! -d keyd ]]; then
+	echo "keyd"
+	git clone "$KEYD_REPO"
+	cd keyd
+	make && sudo make install
+	sudo systemctl enable --now keyd
+	cd ..
+fi
+
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
 	echo "Installing Oh My Zsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+	ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+	mkdir -p "$ZSH_CUSTOM/plugins"
+	if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]]; then
+		# reminder: this needs to be installed AFTER ohmyzsh
+		echo "Cloning Zsh Autosuggestions plugin..."
+		git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+	else
+		echo "Zsh Autosuggestions plugin already installed."
+	fi
+
+	mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
 fi
 
-echo "keyd"
-if [[ ! -d keyd ]]; then
-	git clone "$KEYD_REPO"
-fi
-cd keyd
-make && sudo make install
-sudo systemctl enable --now keyd
-cd ..
-
-echo "Script execution completed successfully!"
+echo "script done"
